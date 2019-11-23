7b84f34fca75c19e4ea40f5b89d9f8ab7d8948f3
Timotei Dumitru <dumitru.timy@gmail.com>
2019-11-23 14:08:10 +0000

    update API Key and package.json / package-lock files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-import Titles from './components/Titles';
-import Form from './components/Form';
-import Weather from './components/Weather';
-import Footer from './components/Footer';
+import Titles from "./components/Titles";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
+import Footer from "./components/Footer";
 
-
-const API_KEY = "f322c16ca21d9c08b54608cf88c9971a";
+const API_KEY = "02e8d71b0387ff5174b1913ab68d9663";
 
 class App extends Component {
   state = {
@@ -18,25 +17,28 @@ class App extends Component {
     description: undefined,
     icon: undefined,
     error: undefined
-  }
+  };
 
   getWeather = async e => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const api_call = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`
+    );
     const data = await api_call.json();
-    if(city && country) {
+    if (city && country) {
       this.setState({
         city: data.name,
         country: data.sys.country,
         temperature: Math.floor(data.main.temp),
         humidity: data.main.humidity,
         description: data.weather[0].main,
-        icon: "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
+        icon:
+          "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png",
         error: ""
-      })
+      });
     } else {
       this.setState({
         temperature: undefined,
@@ -45,10 +47,10 @@ class App extends Component {
         humidity: undefined,
         description: undefined,
         error: "Please enter City and Country!"
-      })
+      });
     }
-    // console.log(data)
-  }
+    console.log(data);
+  };
 
   render() {
     return (
@@ -61,8 +63,8 @@ class App extends Component {
                   <Titles />
                 </div>
                 <div className="form-container">
-                  <Form getWeather={this.getWeather}/>
-                  <Weather 
+                  <Form getWeather={this.getWeather} />
+                  <Weather
                     temperature={this.state.temperature}
                     city={this.state.city}
                     country={this.state.country}
