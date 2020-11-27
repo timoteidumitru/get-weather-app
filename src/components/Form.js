ffaed7344fff5ef55a8c4dc7372d8564212ac33d
Timotei Dumitru <dumitru.timy@gmail.com>
2020-11-27 20:26:05 +0000

    modify the inout colors for a better user experience

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from "react";
 
-const Form = props => (
+const Form = (props) => (
   <div>
     <form className="form-main" onSubmit={props.getWeather}>
-      <input type="text" name="city" placeholder="Enter a city..."/>
-      <input type="text" name="country" placeholder="Enter a country..."/>
+      <input type="text" name="city" placeholder="Enter a city..." />
+      <input type="text" name="country" placeholder="Enter country..." />
       <button>Get Weather</button>
     </form>
   </div>
--- src/components/Titles.js
+++ src/components/Titles.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 const Titles = () => (
   <div>
     <h1 className="title-container__title">Weather Finder</h1>
-    <p className="title-container__subtitle">Find out temperature, conditions and more...</p>
+    <h4 className="title-container__title-fun">
+      Special pentru GEORGI, care are probleme cu vederea.. :))
+    </h4>
+    <p className="title-container__subtitle">
+      Find out temperature, conditions and more...
+    </p>
   </div>
-)
+);
 
 export default Titles;
-
