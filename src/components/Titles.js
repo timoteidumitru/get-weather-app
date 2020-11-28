ec82da16bf8722c2321cef87b21699adfb58f9f7
Timotei Dumitru <dumitru.timy@gmail.com>
2020-11-28 19:45:00 +0000

    modify back the 'fun' article inserted in the title section

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -3,9 +3,9 @@ import React from "react";
 const Titles = () => (
   <div>
     <h1 className="title-container__title">Weather Finder</h1>
-    <h4 className="title-container__title-fun">
+    {/* <h4 className="title-container__title-fun">
       Special pentru GEORGI, care are probleme cu vederea.. :))
-    </h4>
+    </h4> */}
     <p className="title-container__subtitle">
       Find out temperature, conditions and more...
     </p>
